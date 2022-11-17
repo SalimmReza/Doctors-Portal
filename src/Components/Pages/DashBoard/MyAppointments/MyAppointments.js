813d810187841fb88579d329424ee80907d43924
SalimmReza <salimnuaa@gmail.com>
2022-11-17 16:01:41 +0600

    my appointment specific email done

---
+++ src/Components/Pages/DashBoard/MyAppointments/MyAppointments.js
@@ -0,0 +1,60 @@
+import { async } from '@firebase/util';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../../Context/AuthProvider';
+
+const MyAppointments = () => {
+
+    const { user } = useContext(AuthContext)
+
+
+
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+            const data = await res.json()
+            return data;
+        }
+    })
+    return (
+        <div className='p-10'>
+            <h1 className='text-2xl font-bold my-10'>MyAppointments</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Treatment</th>
+                            <th>Time</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookings.map((booking, i) => <tr
+                                key={booking._id}
+                            >
+                                <th>{i + 1}</th>
+                                <td>{booking.patient}</td>
+                                <td>{booking.treatment}</td>
+                                <td>{booking.slot}</td>
+                                <td>{booking.appointmentDate}</td>
+                            </tr>)
+                        }
+
+
+
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyAppointments;
